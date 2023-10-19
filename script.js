5c9b7802a8bb26f6992f8ef95359de3aee5527bf
hugoo2 <97876371+hugoo2@users.noreply.github.com>
2023-10-19 12:49:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,17 +14,28 @@ function startGame() {
     document.getElementById("joueur1Display").textContent = joueur1Name;
     document.getElementById("joueur2Display").textContent = joueur2Name;
 
+    // Récupérer les scores des cookies
+    scores.joueur1 = parseInt(getCookie("joueur1Score") || "0");
+    scores.joueur2 = parseInt(getCookie("joueur2Score") || "0");
+
+    // Mettre à jour l'affichage des scores
+    updateDisplay('joueur1');
+    updateDisplay('joueur2');
+
     document.getElementById("playerInput").style.display = "none";
     document.getElementById("gameArea").style.display = "block";
 }
 
+
 function incrementScore(joueur) {
     scores[joueur]++;
+    setCookie(joueur + "Score", scores[joueur], 7); // Sauvegarde du score dans les cookies
     updateDisplay(joueur);
 }
 
 function decrementScore(joueur) {
-    scores[joueur] = Math.max(0, scores[joueur] - 1);  // pour éviter des valeurs négatives
+    scores[joueur] = Math.max(0, scores[joueur] - 1);
+    setCookie(joueur + "Score", scores[joueur], 7); // Sauvegarde du score dans les cookies
     updateDisplay(joueur);
 }
 
@@ -35,6 +46,19 @@ function updateDisplay(joueur) {
 function changeTeams() {
     document.getElementById("playerInput").style.display = "block";
     document.getElementById("gameArea").style.display = "none";
+}
+
+function resetScores() {
+    const userConfirmed = confirm("Êtes-vous sûr de vouloir réinitialiser les scores?");
+    
+    if (userConfirmed) {
+        scores.joueur1 = 0;
+        scores.joueur2 = 0;
+        setCookie("joueur1Score", 0, 7);
+        setCookie("joueur2Score", 0, 7);
+        updateDisplay('joueur1');
+        updateDisplay('joueur2');
+    }
 }
 
 // Fonctions pour gérer les cookies
@@ -61,41 +85,22 @@ function getCookie(cname) {
     return "";
 }
 
-// Au chargement, vérifiez les cookies pour les noms d'équipe précédemment définis
+// Au chargement, récupérez les noms et scores depuis les cookies
 document.addEventListener('DOMContentLoaded', (event) => {
     var savedJoueur1 = getCookie("joueur1");
     var savedJoueur2 = getCookie("joueur2");
-
-    if (savedJoueur1) {
-        document.getElementById("joueur1Name").value = savedJoueur1;
-    }
-    if (savedJoueur2) {
-        document.getElementById("joueur2Name").value = savedJoueur2;
-    }
-});
-
-
-// Au chargement, vérifiez les cookies pour les noms d'équipe précédemment définis
-document.addEventListener('DOMContentLoaded', (event) => {
-    var savedJoueur1 = getCookie("joueur1");
-    var savedJoueur2 = getCookie("joueur2");
+    var savedJoueur1Score = getCookie("joueur1Score");
+    var savedJoueur2Score = getCookie("joueur2Score");
 
     if (savedJoueur1 && savedJoueur2) {
         document.getElementById("joueur1Name").value = savedJoueur1;
         document.getElementById("joueur2Name").value = savedJoueur2;
-
-        startGame();  // initialiser le jeu avec les noms enregistrés
+        if (savedJoueur1Score) {
+            scores.joueur1 = parseInt(savedJoueur1Score);
+        }
+        if (savedJoueur2Score) {
+            scores.joueur2 = parseInt(savedJoueur2Score);
+        }
+        startGame();
     }
 });
-
-
-function resetScores() {
-    const userConfirmed = confirm("Êtes-vous sûr de vouloir réinitialiser les scores?");
-    
-    if (userConfirmed) {
-        scores.joueur1 = 0;
-        scores.joueur2 = 0;
-        updateDisplay('joueur1');
-        updateDisplay('joueur2');
-    }
-}
